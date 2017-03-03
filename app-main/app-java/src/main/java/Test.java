de8fcb0bf990fbd1ae63f6014116562942f1d55f
chit <chit@chitpc>
2017-03-03 16:52:39 +0800

    add code.

--- app-main/app-java/src/main/java/Test.java
+++ app-main/app-java/src/main/java/Test.java
@@ -1,8 +1,13 @@
 import java.lang.reflect.InvocationHandler;
+import java.util.HashMap;
 
 
 public class Test {
 	public static void main(String[] args) {
-		
+		String s=  " cake";
+		double a = 0.0d;
+		char c,b='a';
+		int[] t = new int[19];
+		System.out.println(t[0]);
 	}
 }
